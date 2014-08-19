81518de76e6201dddac2c9551ab3a1038cdc3551
Isaac Hermens <isaac.hermens@gmail.com>
2014-08-18 17:15:14 -0700

    Added stub api for interfacing with Windows power management services.

---
+++ QuickPower/API/PowerManagement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickPower.API
+{
+    public static class PowerManagement
+    {
+        public static List<PowerScheme> GetPowerSchemes()
+        {
+            return new List<PowerScheme> { new PowerScheme(Guid.NewGuid(), "Low", false), new PowerScheme(Guid.NewGuid(), "Bob", false) };
+        }
+
+        public static void SetActiveScheme(Guid powerSchemeId)
+        {
+            //todo implement
+        }
+    }
+}
---
+++ QuickPower/API/PowerScheme.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickPower.API
+{
+    public class PowerScheme : ICloneable
+    {
+        public string Name { get; set; }
+        public Guid ID { get; set; }
+        public bool Active { get; set; }
+
+        public PowerScheme(Guid id, string name, bool active)
+        {
+            ID = id;
+            Name = name;
+            Active = active;
+        }
+
+        public object Clone()
+        {
+            return new PowerScheme(ID, Name, Active);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var scheme = obj as PowerScheme;
+            return scheme != null && scheme.ID.Equals(this.ID);
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
+    }
+}
